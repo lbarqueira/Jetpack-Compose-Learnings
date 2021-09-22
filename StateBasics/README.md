6ac26f4d499fff525ea0d4b7197d25e4b772c1f1
Luis Barqueira <luis.barqueira@gmail.com>
2021-09-22 23:06:02 +0100

    22 set commit

--- StateBasics/README.md
+++ StateBasics/README.md
@@ -1,1 +1,8 @@
 # Using State in Jetpack Compose
+
+## Important notes to remember:
+
+![Note 1](Images/state_1.png "MutableState")
+
+![Note 2](Images/state_2.png "State hoisting")
+
--- StateBasics/app/src/main/java/com/lbarqueira/statebasics/MainActivity.kt
+++ StateBasics/app/src/main/java/com/lbarqueira/statebasics/MainActivity.kt
@@ -5,10 +5,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -30,10 +27,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun MyComposable() {
+
     var myValue by remember { mutableStateOf(false) }
     Log.d("Recomposition", "Fist")
+
+    val (text, setText) = remember { mutableStateOf("") }
+
+    val isTextVisible = text.isNotBlank()
 
     Column(
         modifier = Modifier
@@ -52,9 +55,45 @@ fun MyComposable() {
         Spacer(modifier = Modifier.height(4.dp))
 
         Text(text = "I am testing state, state is $myValue")
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        TextFieldInput(text, setText, isTextVisible)
+
     }
 
 }
+
+// This pattern is called state hoisting. We "hoist" (or lift) state from a composable
+// to make it stateless. State hoisting is the main pattern to build unidirectional
+// data flow designs in Compose.
+// Extracting a stateless composable from a stateful composable makes it easier to reuse the UI
+// in different locations.
+@Composable
+private fun TextFieldInput(
+    text: String, // the current value to display
+    setText: (String) -> Unit, //  an event that requests the value to change
+    isTextVisible: Boolean
+) {
+    TextField(
+        value = text,
+        onValueChange = {
+            setText(it)
+        },
+        label = { Text("Label") }
+    )
+    if (isTextVisible) {
+        DisplayText(text)
+    } else {
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
+@Composable
+private fun DisplayText(text: String) {
+    Text(text = "I`m just typing: $text")
+}
+
 
 @Preview(showSystemUi = true)
 @Composable
